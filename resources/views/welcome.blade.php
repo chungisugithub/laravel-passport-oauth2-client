a182a75d6ec71afdbe6b8af9b6ddb93cf53e1e72
chungisugithub <chungisu@gmail.com>
2022-10-11 08:31:00 +0900

    change title to OAuth Client

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    OAuth Client
                 </div>
 
                 <div class="links">
