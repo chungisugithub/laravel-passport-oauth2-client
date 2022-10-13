0445a5601cc84862627e59bb6115839fad6e0f39
chungisugithub <chungisu@gmail.com>
2022-10-14 03:46:16 +0900

    리다이렉트, 콜백 라우터 추가

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/oauth/redirect', 'OAuthController@redirect');
+Route::get('/oauth/callback', 'OAuthController@callback');
