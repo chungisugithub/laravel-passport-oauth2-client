fd7ddb4b205fe411ee673a0134ff3bc31488e4b5
chungisugithub <chungisu@gmail.com>
2022-10-14 03:59:46 +0900

    리다이렉트 콜백 테스트

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -7,10 +7,15 @@ use Illuminate\Http\Request;
 class OAuthController extends Controller
 {
     public function redirect() {
-
+        $queries = http_build_query([
+            'client_id' => '5',
+            'redirect_uri'=> 'http://127.0.0.1:8001/oauth/callback',
+            'response_type' => 'code'
+        ]);
+        return redirect('http://127.0.0.1:8000/oauth/authorize?'. $queries);
     }
 
-    public function callback() {
-        
+    public function callback(Request $request) {
+        dd($request->all());
     }
 }
